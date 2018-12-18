f4f5139be12c01960b928c18af63438634a5ab15
ryanhelling <ryan.a.helling@gmail.com>
2018-12-17 23:30:17 -0800

    just trying to make it work

--- event/models.py
+++ event/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-
 # Create your models here.
+from django.db.models import ForeignKey
+from django.contrib.auth.models import User
 
 
 class Event(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
+    author = ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
